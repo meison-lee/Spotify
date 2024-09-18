bedb3344fa367dd99b04cec4180d009bf1f752ac
Meison <b08505012@ntu.edu.tw>
2024-09-18 17:47:11 +0800

    normalize the schema

--- schema.sql
+++ schema.sql
@@ -5,38 +5,52 @@ CREATE TABLE IF NOT EXISTS users (
     userID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_playlists INTEGER[] DEFAULT '{}'
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE IF NOT EXISTS artists (
     artistID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    artist_name VARCHAR(50) NOT NULL,
+    artist_name VARCHAR(50) UNIQUE NOT NULL ,
     email VARCHAR(100) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    albums INTEGER[] DEFAULT '{}'
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS albums (
-    albumID BIGSERIAL PRIMARY KEY,
-    artistID INT NOT NULL,
+    albumID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    artistID UUID REFERENCES artists(artistID) ON DELETE CASCADE,
     album_name VARCHAR(50) NOT NULL,
     album_artwork VARCHAR(100) NOT NULL, -- URL to the album artwork
-    release_date DATE NOT NULL
+    release_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS songs (
-    songID BIGSERIAL PRIMARY KEY,
+    songID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     song_name VARCHAR(50) NOT NULL,
     song_length INT NOT NULL, -- in seconds
     song_url VARCHAR(100) NOT NULL, -- URL to the song
     song_artwork VARCHAR(100) NOT NULL, -- URL to the song artwork
-    albumID INT REFERENCES albums(albumID),
-    artistID UUID REFERENCES artists(artistID)
+    albumID INT REFERENCES albums(albumID) ON DELETE CASCADE,
+    artistID UUID REFERENCES artists(artistID) ON DELETE CASCADE,
 );
 
 CREATE TABLE IF NOT EXISTS playlists (
-    playlistID BIGSERIAL PRIMARY KEY,
+    playlistID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    userID UUID REFERENCES users(userID),
     playlist_name VARCHAR(50) NOT NULL,
-    playlist_songs INTEGER[] DEFAULT '{}',
-    userID UUID REFERENCES users(userID)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS playlist_song (
+    playlistID UUID REFERENCES playlists(playlistID) ON DELETE CASCADE,
+    songID UUID REFERENCES songs(songID) ON DELETE CASCADE,
+    PRIMARY KEY (playlistID, songID)
+);
+
+CREATE TABLE IF NOT EXISTS user_plalist (
+    userID UUID REFERENCES users(userID) ON DELETE CASCADE,
+    playlistID UUID REFERENCES playlists(playlistID) ON DELETE CASCADE,
+    PRIMARY KEY (userID, playlistID)
+);
+
+
+
